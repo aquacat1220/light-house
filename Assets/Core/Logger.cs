43582faf00d07f7c15bcec0b2a441c5e5a1abc66
aquacat1220 <aquacat1220@gmail.com>
2025-09-18 12:17:36 +0900

    fix: fix item callbacks triggering before client init on host scenarios, implement `SingleFire`

---
+++ Assets/Core/Logger.cs
@@ -0,0 +1,25 @@
+using FishNet.Object;
+using UnityEngine;
+
+public class Logger : NetworkBehaviour
+{
+    public void Log(string str)
+    {
+        Debug.Log(str);
+    }
+
+    public override void OnStartNetwork()
+    {
+        Debug.Log("Network");
+    }
+
+    public override void OnStartServer()
+    {
+        Debug.Log("Server");
+    }
+
+    public override void OnStartClient()
+    {
+        Debug.Log("Client");
+    }
+}
