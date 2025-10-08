1510d0103b3f6b2f2d5b6c02249703da849d4557
aquacat1220 <aquacat1220@gmail.com>
2025-10-09 05:59:27 +0900

    feat: switch all weapons to use pulse based inputs

---
+++ Assets/Core/Utils/NetworkLogger.cs
@@ -0,0 +1,41 @@
+using FishNet.Connection;
+using FishNet.Object;
+using UnityEngine;
+
+public class NetworkLogger : NetworkBehaviour
+{
+    public override void OnStartServer()
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Started as server.");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Stopped as server.");
+    }
+
+    public override void OnStartClient()
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Started as client.");
+    }
+
+    public override void OnStopClient()
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Stopped as client.");
+    }
+
+    public override void OnOwnershipServer(NetworkConnection prevOwner)
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Ownership change on server. Previous owner: {prevOwner}.");
+    }
+
+    public override void OnOwnershipClient(NetworkConnection prevOwner)
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] Ownership change on client. Previous owner: {prevOwner}.");
+    }
+
+    public void Log(string str)
+    {
+        Debug.Log($"[Time: {Time.time} | Owner: {base.Owner} | ServerInit: {base.IsServerInitialized} | ClientInit: {base.IsClientInitialized}] {str}");
+    }
+}
