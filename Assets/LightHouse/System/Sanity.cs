40935227e1784c9ff00a5c5bdb7cb70101d876e1
aquacat1220 <aquacat1220@gmail.com>
2025-10-24 09:39:18 +0900

    feat: implement vitality sanity components

---
+++ Assets/LightHouse/System/Sanity.cs
@@ -0,0 +1,60 @@
+using System;
+using FishNet.Object;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Sanity : NetworkBehaviour
+{
+    [SerializeField]
+    [MinMaxSlider(-100f, 500f)]
+    Vector2 _minMaxSan = new Vector2(0f, 100f);
+
+    [SerializeField]
+    [Min(0f)]
+    float _initialSan = 100.0f;
+
+    [SerializeField]
+    UnityEvent<float> _sanChange;
+    [SerializeField]
+    UnityEvent _sanBelowZero;
+
+    float _san = 0f;
+    public float San
+    {
+        get { return _san; }
+    }
+
+    void Awake()
+    {
+        _san = Math.Clamp(_initialSan, _minMaxSan.x, _minMaxSan.y);
+        _sanChange?.Invoke(_san);
+        if (_san <= 0f)
+            _sanBelowZero?.Invoke();
+    }
+
+    [Server]
+    public void ApplyDamage(float damage, bool canHeal = false)
+    {
+        if (!canHeal)
+            damage = Math.Max(damage, 0f);
+        var oldSan = _san;
+        _san = Math.Clamp(_san - damage, _minMaxSan.x, _minMaxSan.y);
+        if (oldSan != _san)
+        {
+            _sanChange?.Invoke(_san);
+            if (_san <= 0f)
+                _sanBelowZero?.Invoke();
+            SanChangeRpc(_san);
+        }
+    }
+
+    [ObserversRpc(ExcludeServer = true, BufferLast = true)]
+    void SanChangeRpc(float newSan)
+    {
+        _san = newSan;
+        _sanChange?.Invoke(_san);
+        if (_san <= 0f)
+            _sanBelowZero?.Invoke();
+    }
+}
---
+++ Assets/LightHouse/System/Vitality.cs
@@ -0,0 +1,60 @@
+using System;
+using FishNet.Object;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Vitality : NetworkBehaviour
+{
+    [SerializeField]
+    [MinMaxSlider(-100f, 500f)]
+    Vector2 _minMaxVit = new Vector2(0f, 100f);
+
+    [SerializeField]
+    [Min(0f)]
+    float _initialVit = 100.0f;
+
+    [SerializeField]
+    UnityEvent<float> _vitChange;
+    [SerializeField]
+    UnityEvent _vitBelowZero;
+
+    float _vit = 0f;
+    public float Vit
+    {
+        get { return _vit; }
+    }
+
+    void Awake()
+    {
+        _vit = Math.Clamp(_initialVit, _minMaxVit.x, _minMaxVit.y);
+        _vitChange?.Invoke(_vit);
+        if (_vit <= 0f)
+            _vitBelowZero?.Invoke();
+    }
+
+    [Server]
+    public void ApplyDamage(float damage, bool canHeal = false)
+    {
+        if (!canHeal)
+            damage = Math.Max(damage, 0f);
+        var oldVit = _vit;
+        _vit = Math.Clamp(_vit - damage, _minMaxVit.x, _minMaxVit.y);
+        if (oldVit != _vit)
+        {
+            _vitChange?.Invoke(_vit);
+            if (_vit <= 0f)
+                _vitBelowZero?.Invoke();
+            VitChangeRpc(_vit);
+        }
+    }
+
+    [ObserversRpc(ExcludeServer = true, BufferLast = true)]
+    void VitChangeRpc(float newVit)
+    {
+        _vit = newVit;
+        _vitChange?.Invoke(_vit);
+        if (_vit <= 0f)
+            _vitBelowZero?.Invoke();
+    }
+}
