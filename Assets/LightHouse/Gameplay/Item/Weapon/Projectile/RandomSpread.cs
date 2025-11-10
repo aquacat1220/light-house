41981447676cc5106a1109f10c6c760497b2937a
aquacat1220 <aquacat1220@gmail.com>
2025-11-11 00:07:58 +0900

    chore: wrap `RandomSpread` in lighthouse namespace

--- Assets/LightHouse/Gameplay/Item/Weapon/Projectile/RandomSpread.cs
+++ Assets/LightHouse/Gameplay/Item/Weapon/Projectile/RandomSpread.cs
@@ -1,169 +1,171 @@
-using System;
-using FishNet.Object;
-using LightHouse;
-using NaughtyAttributes;
-using UnityEngine;
-
-public class RandomSpread : NetworkBehaviour
+namespace LightHouse
 {
-    [SerializeField]
-    [Required]
-    [ValidateInput("CheckSpawn", "`_spawnPoint` should have a zeroed local transform.")]
-    Transform _spawnPoint;
+    using System;
+    using FishNet.Object;
+    using NaughtyAttributes;
+    using UnityEngine;
 
-    // Weapon variance (inaccuracy due to weapon design) in degrees.
-    [SerializeField]
-    [Min(0f)]
-    float _weaponVariance = 1f;
-    // A curve that maps heat to weapon variance modifier (multiplied to weapon variance).
-    [SerializeField]
-    [CurveRange(0f, 0f, 1f, 1f)]
-    AnimationCurve _weaponVarianceModifierCurve;
-
-    // Aiming variance (inaccuracy from character's imprecise aiming) in degrees.
-    [SerializeField]
-    [Min(0f)]
-    float _aimVariance = 1f;
-    // Modifier to be multiplied to aim variance.
-    [SerializeField]
-    [Min(0f)]
-    float _aimVarianceModifier = 1f;
-
-    // Accumulated heat per fire.
-    [SerializeField]
-    [Min(0f)]
-    float _heatPerFire = 0.25f;
-    // Delay (in seconds) before a heated weapon starts cooling down.
-    [SerializeField]
-    [Min(0f)]
-    float _coolDelay = 1f;
-    // Amount of heat cooled per second.
-    [SerializeField]
-    float _coolPerSecond = 0.25f;
-
-    float _heat = 0f;
-    float _lastAimError = 0f;
-
-    int _predictedCounter = 0;
-
-    Alarm _delayAlarm;
-    Alarm _coolAlarm;
-
-    void Awake()
+    public class RandomSpread : NetworkBehaviour
     {
-        if (_spawnPoint == null)
+        [SerializeField]
+        [Required]
+        [ValidateInput("CheckSpawn", "`_spawnPoint` should have a zeroed local transform.")]
+        Transform _spawnPoint;
+
+        // Weapon variance (inaccuracy due to weapon design) in degrees.
+        [SerializeField]
+        [Min(0f)]
+        float _weaponVariance = 1f;
+        // A curve that maps heat to weapon variance modifier (multiplied to weapon variance).
+        [SerializeField]
+        [CurveRange(0f, 0f, 1f, 1f)]
+        AnimationCurve _weaponVarianceModifierCurve;
+
+        // Aiming variance (inaccuracy from character's imprecise aiming) in degrees.
+        [SerializeField]
+        [Min(0f)]
+        float _aimVariance = 1f;
+        // Modifier to be multiplied to aim variance.
+        [SerializeField]
+        [Min(0f)]
+        float _aimVarianceModifier = 1f;
+
+        // Accumulated heat per fire.
+        [SerializeField]
+        [Min(0f)]
+        float _heatPerFire = 0.25f;
+        // Delay (in seconds) before a heated weapon starts cooling down.
+        [SerializeField]
+        [Min(0f)]
+        float _coolDelay = 1f;
+        // Amount of heat cooled per second.
+        [SerializeField]
+        float _coolPerSecond = 0.25f;
+
+        float _heat = 0f;
+        float _lastAimError = 0f;
+
+        int _predictedCounter = 0;
+
+        Alarm _delayAlarm;
+        Alarm _coolAlarm;
+
+        void Awake()
         {
-            Debug.Log("`_spawnPoint` was not set.");
-            throw new Exception();
-        }
-        if (_spawnPoint.localPosition != Vector3.zero || _spawnPoint.localRotation != Quaternion.identity || _spawnPoint.localScale != Vector3.one)
-        {
-            Debug.Log("`_spawnPoint` should have a zeroed local transform.");
-            throw new Exception();
-        }
-    }
-
-    public override void OnStartNetwork()
-    {
-        _delayAlarm = TimerManager.Singleton.AddAlarm(
-            cooldown: _coolDelay,
-            callback: StartCooling,
-            startImmediately: false,
-            armImmediately: true,
-            autoRestart: false,
-            autoRearm: true,
-            initialCooldown: _coolDelay,
-            destroyAfterTriggered: false
-        );
-        OnPredictedCounterChange(_predictedCounter);
-    }
-
-    public override void OnStopNetwork()
-    {
-        _delayAlarm.Remove();
-        _coolAlarm?.Remove();
-    }
-
-    void Update()
-    {
-        // Arm alarm every frame, so the alarm will trigger on every frame as long as it is started.
-        _coolAlarm?.Arm();
-    }
-
-    public void ApplySpread()
-    {
-        bool addHeat = true; bool reuseAimError = false;
-        (var aimGaussian, var weaponGaussian) = new SplitMix64((ulong)_predictedCounter).NextGaussian();
-
-        float aimError = _lastAimError;
-        if (!reuseAimError)
-        {
-            float aimVariance = _aimVariance * _aimVarianceModifier;
-            aimError = (float)aimGaussian * aimVariance;
-            _lastAimError = aimError;
-            Debug.Log($"{Time.time}: Recalculated aim error to {aimError}.");
+            if (_spawnPoint == null)
+            {
+                Debug.Log("`_spawnPoint` was not set.");
+                throw new Exception();
+            }
+            if (_spawnPoint.localPosition != Vector3.zero || _spawnPoint.localRotation != Quaternion.identity || _spawnPoint.localScale != Vector3.one)
+            {
+                Debug.Log("`_spawnPoint` should have a zeroed local transform.");
+                throw new Exception();
+            }
         }
 
-        float weaponVariance = _weaponVariance * _weaponVarianceModifierCurve.Evaluate(_heat);
-        float weaponError = (float)weaponGaussian * weaponVariance;
-
-        float error = aimError + weaponError;
-        Debug.Log($"{Time.time}: Set spawn point with aim error {aimError}, weapon error {weaponError}.");
-        _spawnPoint.localEulerAngles = new Vector3(0f, 0f, error);
-
-        if (addHeat)
+        public override void OnStartNetwork()
         {
-            _heat = Math.Clamp(_heat + _heatPerFire, 0f, 1f);
-            Debug.Log($"{Time.time}: Heated up to {_heat}.");
+            _delayAlarm = TimerManager.Singleton.AddAlarm(
+                cooldown: _coolDelay,
+                callback: StartCooling,
+                startImmediately: false,
+                armImmediately: true,
+                autoRestart: false,
+                autoRearm: true,
+                initialCooldown: _coolDelay,
+                destroyAfterTriggered: false
+            );
+            OnPredictedCounterChange(_predictedCounter);
         }
-        _coolAlarm?.Remove();
-        _coolAlarm = null;
-        _delayAlarm?.Reset(_coolDelay);
-        _delayAlarm?.Start();
-    }
 
-    public void OnPredictedCounterChange(int newPredictedCounter)
-    {
-        _predictedCounter = newPredictedCounter;
-    }
-
-    void StartCooling(float _)
-    {
-        Debug.Log($"{Time.time}: Starting cooling.");
-        if (_coolAlarm != null)
+        public override void OnStopNetwork()
         {
-            Debug.Log("We already have a cool alarm, which shouldn't be possible.");
-            throw new Exception();
+            _delayAlarm.Remove();
+            _coolAlarm?.Remove();
         }
-        _coolAlarm = TimerManager.Singleton.AddAlarm(
-            cooldown: 0f,
-            callback: Cool,
-            startImmediately: true,
-            armImmediately: true,
-            autoRestart: true,
-            autoRearm: false,
-            initialCooldown: 0f,
-            destroyAfterTriggered: false
-        );
-    }
 
-    void Cool(float deltaTime)
-    {
-        _heat = Math.Clamp(_heat - deltaTime * _coolPerSecond, 0f, 1f);
-        Debug.Log($"{Time.time}: Cooled down to {_heat}.");
-        if (_heat == 0f)
+        void Update()
         {
-            _coolAlarm.Remove();
+            // Arm alarm every frame, so the alarm will trigger on every frame as long as it is started.
+            _coolAlarm?.Arm();
+        }
+
+        public void ApplySpread()
+        {
+            bool addHeat = true; bool reuseAimError = false;
+            (var aimGaussian, var weaponGaussian) = new SplitMix64((ulong)_predictedCounter).NextGaussian();
+
+            float aimError = _lastAimError;
+            if (!reuseAimError)
+            {
+                float aimVariance = _aimVariance * _aimVarianceModifier;
+                aimError = (float)aimGaussian * aimVariance;
+                _lastAimError = aimError;
+                Debug.Log($"{Time.time}: Recalculated aim error to {aimError}.");
+            }
+
+            float weaponVariance = _weaponVariance * _weaponVarianceModifierCurve.Evaluate(_heat);
+            float weaponError = (float)weaponGaussian * weaponVariance;
+
+            float error = aimError + weaponError;
+            Debug.Log($"{Time.time}: Set spawn point with aim error {aimError}, weapon error {weaponError}.");
+            _spawnPoint.localEulerAngles = new Vector3(0f, 0f, error);
+
+            if (addHeat)
+            {
+                _heat = Math.Clamp(_heat + _heatPerFire, 0f, 1f);
+                Debug.Log($"{Time.time}: Heated up to {_heat}.");
+            }
+            _coolAlarm?.Remove();
             _coolAlarm = null;
+            _delayAlarm?.Reset(_coolDelay);
+            _delayAlarm?.Start();
         }
-    }
 
-    bool CheckSpawn(Transform spawnPoint)
-    {
-        if (_spawnPoint == null)
+        public void OnPredictedCounterChange(int newPredictedCounter)
+        {
+            _predictedCounter = newPredictedCounter;
+        }
+
+        void StartCooling(float _)
+        {
+            Debug.Log($"{Time.time}: Starting cooling.");
+            if (_coolAlarm != null)
+            {
+                Debug.Log("We already have a cool alarm, which shouldn't be possible.");
+                throw new Exception();
+            }
+            _coolAlarm = TimerManager.Singleton.AddAlarm(
+                cooldown: 0f,
+                callback: Cool,
+                startImmediately: true,
+                armImmediately: true,
+                autoRestart: true,
+                autoRearm: false,
+                initialCooldown: 0f,
+                destroyAfterTriggered: false
+            );
+        }
+
+        void Cool(float deltaTime)
+        {
+            _heat = Math.Clamp(_heat - deltaTime * _coolPerSecond, 0f, 1f);
+            Debug.Log($"{Time.time}: Cooled down to {_heat}.");
+            if (_heat == 0f)
+            {
+                _coolAlarm.Remove();
+                _coolAlarm = null;
+            }
+        }
+
+        bool CheckSpawn(Transform spawnPoint)
+        {
+            if (_spawnPoint == null)
+                return true;
+            if (_spawnPoint.localPosition != Vector3.zero || _spawnPoint.localRotation != Quaternion.identity || _spawnPoint.localScale != Vector3.one)
+                return false;
             return true;
-        if (_spawnPoint.localPosition != Vector3.zero || _spawnPoint.localRotation != Quaternion.identity || _spawnPoint.localScale != Vector3.one)
-            return false;
-        return true;
+        }
     }
 }
