a2224b847e7b8eef3b73dec66c31db8030ba4357
aquacat1220 <aquacat1220@gmail.com>
2025-12-06 10:41:43 +0900

    feat: expose aimline length and width as properties

--- Assets/LightHouse/Gameplay/Item/Weapon/Shared/AimLine/AimLineController.cs
+++ Assets/LightHouse/Gameplay/Item/Weapon/Shared/AimLine/AimLineController.cs
@@ -18,12 +18,33 @@ namespace LightHouse
         // Pixels per unit. Should be set in Assets/LightHouse/System/UI/Setting/WorldPanelSettings.asset, but unity doesn't expose the corresponding property.
         [SerializeField]
         float _pixelsPerUnit = 100f;
+
         // The intended aimline length in world units.
         [SerializeField]
         float _aimLineLength = 1f;
+        public float AimLineLength
+        {
+            get => _aimLineLength;
+            set => _aimLineLength = value;
+        }
+
         // The intended aimline width in world units.
         [SerializeField]
         float _aimLineWidth = 0.02f;
+        public float AimLineWidth
+        {
+            get => _aimLineWidth;
+            set => _aimLineWidth = value;
+        }
+
+        // The intended stub length in world units.
+        [SerializeField]
+        float _stubLength = 0.02f;
+        public float StubLength
+        {
+            get => _stubLength;
+            set => _stubLength = value;
+        }
 
         AimLine _aimLine;
 
@@ -42,14 +63,35 @@ namespace LightHouse
             }
         }
 
+        [Button]
+        void ApplyStyle()
+        {
+            if (_aimLineDocument == null)
+            {
+                Debug.Log("`_aimLineDocument` wasn't set.");
+                throw new Exception();
+            }
+            _aimLine = _aimLineDocument.rootVisualElement.Q<AimLine>(className: "aim-line");
+            if (_aimLine != null)
+            {
+                _aimLine.InnerAngle = _randomSpread.AimVariance;
+                _aimLine.OuterAngle = _randomSpread.WeaponVariance;
+                _aimLine.LineLength = AimLineLength * _pixelsPerUnit;
+                _aimLine.LineWidth = AimLineWidth * _pixelsPerUnit;
+                _aimLine.StubLength = StubLength * _pixelsPerUnit;
+            }
+            _aimLine = null;
+        }
+
         void Update()
         {
             if (_aimLine != null)
             {
                 _aimLine.InnerAngle = _randomSpread.AimVariance;
                 _aimLine.OuterAngle = _randomSpread.WeaponVariance;
-                _aimLine.LineLength = _aimLineLength * _pixelsPerUnit;
-                _aimLine.LineWidth = _aimLineWidth * _pixelsPerUnit;
+                _aimLine.LineLength = AimLineLength * _pixelsPerUnit;
+                _aimLine.LineWidth = AimLineWidth * _pixelsPerUnit;
+                _aimLine.StubLength = StubLength * _pixelsPerUnit;
             }
         }
     }
