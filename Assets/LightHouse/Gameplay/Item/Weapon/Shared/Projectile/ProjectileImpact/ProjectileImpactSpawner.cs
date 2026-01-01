ed73514a123e6e55f90e24f2e05cfd7054ac78e1
aquacat1220 <aquacat1220@gmail.com>
2026-01-02 04:26:35 +0900

    feat: destroy projectile impacts on alarm

--- Assets/LightHouse/Gameplay/Item/Weapon/Shared/Projectile/ProjectileImpact/ProjectileImpactSpawner.cs
+++ Assets/LightHouse/Gameplay/Item/Weapon/Shared/Projectile/ProjectileImpact/ProjectileImpactSpawner.cs
@@ -1,6 +1,5 @@
 namespace LightHouse
 {
-    using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.Assertions;
 
@@ -11,6 +10,9 @@ namespace LightHouse
 
         [SerializeField]
         GameObject _projectileImpactPrefab;
+        [SerializeField]
+        float _lifetime = 2f;
+
 
         void Awake()
         {
@@ -31,7 +33,18 @@ namespace LightHouse
                 return;
             var hit = Physics2D.Raycast(transform.position, transform.up);
             var rotZ = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg - 90f;
-            Instantiate(_projectileImpactPrefab, hit.point, Quaternion.Euler(0f, 0f, rotZ));
+            var projectileImpact = Instantiate(_projectileImpactPrefab, hit.point, Quaternion.Euler(0f, 0f, rotZ));
+
+            TimerManager.Singleton.AddAlarm(
+                cooldown: _lifetime,
+                callback: (_) => Destroy(projectileImpact),
+                startImmediately: true,
+                armImmediately: true,
+                autoRestart: false,
+                autoRearm: false,
+                initialCooldown: _lifetime,
+                destroyAfterTriggered: true
+            );
         }
     }
 }
